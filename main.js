b7f7363952bb276ba436183944d81e67e7667671
laosaobao <654280831@qq.com>
2022-06-28 17:36:02 +0800

    home分支

--- main.js
+++ main.js
@@ -19,3 +19,40 @@ export function createApp() {
   }
 }
 // #endif
+// 按需导入 $http 对象
+import { $http } from '@escook/request-miniprogram'
+// 将按需导入的 $http 挂载到 wx 顶级对象之上，方便全局调用
+// wx.$http = $http
+
+// 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
+uni.$http = $http
+$http.beforeRequest=function(options){
+	uni.showLoading({
+		title:'数据加载中...'
+	})
+}
+$http.afterRequest=function(response){
+	// console.log(response)
+	uni.hideLoading()
+	if(response.statusCode!=200)
+	{
+		uni.showToast({
+			title:'请求出错',
+			duration:1500
+		})
+	}
+	else{
+		return response.data
+	}
+	
+}
+$http.baseUrl='https://www.uinav.com'
+
+//toast方法
+uni.$showMsg=function(title='数据请求失败！',duration=1500){
+	uni.showToast({
+		title,
+		duration,
+	})
+	
+}
