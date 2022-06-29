02053fb83dce1665e6189f1910d735d9bedac788
laosaobao <654280831@qq.com>
2022-06-29 21:50:10 +0800

    搜索功能

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,30 +1,36 @@
 <template>
-	<view class="scrollview-contain">
-		<scroll-view scroll-y="true" class="left-scroll-view" :style="{height:wh+'px'}">
-			<block v-for="(item,index) in cateList" :key="index">
-				<view :class="['left-scroll-view-item',index===active?'active':'']" @click="activeChanged(index)">
-					{{item.cat_name}}
-				</view>
-			</block>
+	<view>
+		<my-search @click="gotoSearch"></my-search>
 
-		</scroll-view>
-		<scroll-view scroll-y="true" :style="{height: wh + 'px'}" :scroll-top="scrollTop">
-			<view class="cate-lv2" v-for="(item2, i2) in cateLevel2" :key="i2">
-				<view class="cate-lv2-title">/ {{item2.cat_name}} /</view>
-				<!-- 动态渲染三级分类的列表数据 -->
-				<view class="cate-lv3-list">
-					<!-- 三级分类 Item 项 -->
-					<view class="cate-lv3-item" v-for="(item3, i3) in item2.children" :key="i3" @click="gotoGoodsList(item3)">
-						<!-- 图片 -->
-						<image :src="item3.cat_icon"></image>
-						<!-- 文本 -->
-						<text>{{item3.cat_name}}</text>
+		<view class="scrollview-contain">
+			<scroll-view scroll-y="true" class="left-scroll-view" :style="{height:wh+'px'}">
+				<block v-for="(item,index) in cateList" :key="index">
+					<view :class="['left-scroll-view-item',index===active?'active':'']" @click="activeChanged(index)">
+						{{item.cat_name}}
+					</view>
+				</block>
+
+			</scroll-view>
+			<scroll-view scroll-y="true" :style="{height: wh + 'px'}" :scroll-top="scrollTop">
+				<view class="cate-lv2" v-for="(item2, i2) in cateLevel2" :key="i2">
+					<view class="cate-lv2-title">/ {{item2.cat_name}} /</view>
+					<!-- 动态渲染三级分类的列表数据 -->
+					<view class="cate-lv3-list">
+						<!-- 三级分类 Item 项 -->
+						<view class="cate-lv3-item" v-for="(item3, i3) in item2.children" :key="i3"
+							@click="gotoGoodsList(item3)">
+							<!-- 图片 -->
+							<image :src="item3.cat_icon"></image>
+							<!-- 文本 -->
+							<text>{{item3.cat_name}}</text>
+						</view>
 					</view>
 				</view>
-			</view>
 
-		</scroll-view>
+			</scroll-view>
+	
 	</view>
+</view>
 </template>
 
 <script>
@@ -37,7 +43,7 @@
 				active: 0,
 				// 二级分类列表
 				cateLevel2: [],
-				scrollTop:0
+				scrollTop: 0
 			}
 		},
 		onLoad() {
@@ -46,7 +52,7 @@
 			// 获取当前系统的信息
 			const sysInfo = uni.getSystemInfoSync()
 			// 为 wh 窗口可用高度动态赋值
-			this.wh = sysInfo.windowHeight
+			this.wh = sysInfo.windowHeight-50
 		},
 		methods: {
 			async getCateList() {
@@ -67,14 +73,21 @@
 				// 为二级分类列表重新赋值
 				this.cateLevel2 = this.cateList[i].children
 				// 让 scrollTop 的值在 0 与 1 之间切换
-				  this.scrollTop = this.scrollTop === 0 ? 1 : 0
+				this.scrollTop = this.scrollTop === 0 ? 1 : 0
 			},
 			// 点击三级分类项跳转到商品列表页面
 			gotoGoodsList(item3) {
-			  uni.navigateTo({
-			    url: '/subpkg/goods_list/goods_list?cid=' + item3.cat_id
-			  })
-			}
+				uni.navigateTo({
+					url: '/subpkg/goods_list/goods_list?cid=' + item3.cat_id
+				})
+			},
+			 // 跳转到分包中的搜索页面
+			   gotoSearch() {
+			     uni.navigateTo({
+			       url: '/subpkg/search/search'
+			     })
+			   }
+			
 		}
 	}
 </script>
